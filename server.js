9c89b1efcaf95df4e2565b7d3696403b3a312ec0
Chiragrawat7 <rawatchirag72@gmail.com>
2025-08-11 18:51:38 +0530

    update

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express=require('express');
 const app=express();
 const cookieParser = require("cookie-parser");
 app.use(express.json());
+const cors = require("cors");
 
 require('dotenv').config();
-const port=process.env.PORT;
+const port=process.env.PORT || 3000;
 
 app.use(cookieParser())
 
@@ -14,6 +15,10 @@ const userRoutes=require('./routes/User')
 const contactRoutes=require('./routes/Contact')
 const adminRoutes=require('./routes/Admin')
 
+app.use(cors({
+  origin: "*", // or your frontend URL
+  credentials: true
+}));
 
 app.use('/api/v1',authRoutes);
 app.use('/api/v1/user',userRoutes)
