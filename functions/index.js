d6964cb71220d84391e7c88b7ef80e1cbedbadd2
sangayya mathapati <sangayyam28@gmail.com>
2025-08-12 19:08:29 +0530

    Add updated functions/index.js and firebase.json

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,19 @@
-const functions = require('firebase-functions');
-const express = require('express');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+
+admin.initializeApp();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from Firebase!');
+// Middleware
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// Sample API route
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Firebase Functions + Express!" });
 });
 
-exports.api = functions.https.onRequest(app);
-
+// Export the Express app as a Cloud Function
+exports.app = functions.https.onRequest(app);
