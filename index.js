4613a35682ca47204a5c5479569aa67b26c5f3ac
sangayya mathapati <sangayyam28@gmail.com>
2025-08-19 20:17:36 +0530

    Add Firebase CI/CD workflow with dynamic API URL output

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const functions = require("firebase-functions"); 
-const express = require("express"); 
-const app = express(); 
-const server = require("./server"); 
-app.use(express.json()); 
-app.use(server); 
-exports.app = functions.https.onRequest(app); 
+const functions = require("firebase-functions");
+const app = require("./server");         // your Express app exported below
+exports.app = functions.https.onRequest(app);
+
